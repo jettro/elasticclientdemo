ded7c1704326dd20cdd35d751b8347a8d9c88e31
Jettro Coenradie <jettro@gridshore.nl>
2016-09-20 16:57:44 +0200

    Update to angular 2

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
+import {MdIconRegistry} from "@angular2-material/icon";
 
 @Component({
     selector: 'my-app',
     styles: [require('./app.css')],
     template: require('./app.html'),
-    directives: []
+    providers: [MdIconRegistry]
 })
 export class AppComponent {}
